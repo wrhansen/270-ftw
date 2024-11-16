e1e10c0e6263e26ae6b2dba298019232bb3695d8
Wesley Hansen <wesrhansen@gmail.com>
2024-11-16 14:25:03 -0500

    add 2024 actual map

--- src/data/maps.tsx
+++ src/data/maps.tsx
@@ -1,5 +1,6 @@
 import Current2024Map from "./maps/Current2024Map";
 import BlankMap from "./maps/BlankMap";
+import Actual2024Map from "./maps/Actual2024Map";
 
 export interface MapOption {
     readonly value: string;
@@ -8,6 +9,7 @@ export interface MapOption {
 }
 
 export const mapOptions: readonly MapOption[] = [
+    { value: "Actual2024Map", label: "2024 Actual", data: Actual2024Map },
     { value: 'Current2024Map', label: 'Current 2024 Map', data: Current2024Map },
     { value: 'BlankMap', label: 'Blank Map', data: BlankMap },
 ]
---
+++ src/data/maps/Actual2024Map.tsx
@@ -0,0 +1,219 @@
+import StateColors from "../../models/StateColors";
+
+const Actual2024Map = {
+    "AL": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 9
+    },
+    "AK": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 3
+    },
+    "AZ": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 11
+    },
+    "AR": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 6
+    },
+    "CA": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 54
+    },
+    "CO": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 10
+    },
+    "CT": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 7
+    },
+    "DE": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 3
+    },
+    "DC": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 3
+    },
+    "FL": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 30
+    },
+    "GA": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 16
+    },
+    "HI": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 4
+    },
+    "ID": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 4
+    },
+    "IL": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 19
+    },
+    "IN": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 11
+    },
+    "IA": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 6
+    },
+    "KS": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 6
+    },
+    "KY": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 8
+    },
+    "LA": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 8
+    },
+    "ME": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 2,
+        "splits": [
+            { "fill": StateColors.DemocratSafe, "votes": 1 },
+            { "fill": StateColors.RepublicanSafe, "votes": 1 },
+        ]
+    },
+    "MD": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 10
+    },
+    "MA": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 11
+    },
+    "MI": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 15
+    },
+    "MN": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 10
+    },
+    "MS": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 6
+    },
+    "MO": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 10
+    },
+    "MT": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 4
+    },
+    "NE": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 2,
+        "splits": [
+            { "fill": StateColors.RepublicanSafe, "votes": 1 },
+            { "fill": StateColors.DemocratSafe, "votes": 1 },
+            { "fill": StateColors.RepublicanSafe, "votes": 1 }
+        ]
+    },
+    "NV": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 6
+    },
+    "NH": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 4
+    },
+    "NJ": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 14
+    },
+    "NM": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 5
+    },
+    "NY": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 28
+    },
+    "NC": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 16
+    },
+    "ND": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 3
+    },
+    "OH": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 17
+    },
+    "OK": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 7
+    },
+    "OR": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 8
+    },
+    "PA": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 19
+    },
+    "RI": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 4
+    },
+    "SC": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 9
+    },
+    "SD": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 3
+    },
+    "TN": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 11
+    },
+    "TX": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 40
+    },
+    "UT": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 6
+    },
+    "VT": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 3
+    },
+    "VA": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 13
+    },
+    "WA": {
+        "fill": StateColors.DemocratSafe,
+        "votes": 12
+    },
+    "WV": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 4
+    },
+    "WI": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 10
+    },
+    "WY": {
+        "fill": StateColors.RepublicanSafe,
+        "votes": 3
+    }
+};
+
+export default Actual2024Map;
